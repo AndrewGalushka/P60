b20cb0b641f0e6e5422b874eac68ea2e60010143
AndrewGalushka <andrewgalushka@yahoo.com>
2018-03-24 16:27:34 +0200

    Added MainContentViewController

---
+++ P60/P60/Source/ViewControllers/MainContentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainContentViewController.swift
+//  P60
+//
+//  Created by galushka on 3/24/18.
+//  Copyright © 2018 AndrewGalushka. All rights reserved.
+//
+
+import UIKit
+
+class MainContentViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
