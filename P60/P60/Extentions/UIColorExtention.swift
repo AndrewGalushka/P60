74ff3a454404a12850682411299745ebe345af98
AndrewGalushka <andrewgalushka@yahoo.com>
2018-05-08 17:12:29 +0300

    rf

--- P60/P60/Extentions/UIColorExtention.swift
+++ P60/P60/Extentions/UIColorExtention.swift
@@ -3,4 +3,13 @@
 // Copyright (c) 2018 AndrewGalushka. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static var randomColor: UIColor {
+        return UIColor(red: CGFloat(arc4random() % 255) / 255.0,
+                green: CGFloat(arc4random() % 255) / 255.0,
+                blue: CGFloat(arc4random() % 255) / 255.0,
+                alpha: 1)
+    }
+}
--- P60/P60/Source/Core/Coordinator/ApplicationCoordinator.swift
+++ P60/P60/Source/Core/Coordinator/ApplicationCoordinator.swift
@@ -1,5 +1,5 @@
 //
-//  MainCoordinator.swift
+//  ApplicationCoordinator.swift
 //  P60
 //
 //  Created by galushka on 5/8/18.
@@ -9,6 +9,38 @@
 import UIKit
 
 
-class MainCoordinator {
+class ApplicationCoordinator: Coordinator {
 
+    let window: UIWindow
+    let rootViewController: BaseTabBarController
+
+    init(window: UIWindow) {
+        self.window = window
+
+        rootViewController = BaseTabBarController()
+    }
+
+    func start() {
+
+        let mainStoryBoard = UIStoryboard.init(.main)
+
+        let mainContentViewController: MainContentViewController = mainStoryBoard.instantiateViewController()
+        let settingsViewController: SettingsViewController = mainStoryBoard.instantiateViewController()
+
+        let mainContentViewControllerTabBarItem = UITabBarItem(title: "Main", image: nil, selectedImage: nil)
+        mainContentViewControllerTabBarItem.badgeValue = String(0)
+        mainContentViewControllerTabBarItem.badgeColor = .randomColor
+
+        let settingsViewControllerTabBarItem = UITabBarItem(title: "Settings", image: nil, selectedImage: nil)
+        settingsViewControllerTabBarItem.badgeValue = String(2)
+        settingsViewControllerTabBarItem.badgeColor = .randomColor
+
+        mainContentViewController.tabBarItem = mainContentViewControllerTabBarItem
+        settingsViewController.tabBarItem = settingsViewControllerTabBarItem
+
+        rootViewController.viewControllers = [mainContentViewController, settingsViewController]
+
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+    }
 }
