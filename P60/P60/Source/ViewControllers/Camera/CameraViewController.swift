67ceff4c377a938f425983a61dcf9fd6f5de0840
AndrewGalushka <andrewgalushka@yahoo.com>
2018-05-10 19:36:08 +0300

    rf

--- P60/P60/Source/ViewControllers/Camera/CameraViewController.swift
+++ P60/P60/Source/ViewControllers/Camera/CameraViewController.swift
@@ -7,29 +7,113 @@
 //
 
 import UIKit
+import AVFoundation
+
+enum CameraPosition {
+    case front
+    case rear
+}
 
 class CameraViewController: BaseViewController {
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
+    var currentCameraPosition: CameraPosition?
+    var frontCamera: AVCaptureDevice?
+    var rearCamera: AVCaptureDevice?
+
+    var rearCameraInput: AVCaptureDeviceInput?
+    var frontCameraInput: AVCaptureDeviceInput?
+
+    init() {
+        super.init(nibName: CameraViewController.identifier, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        prepareCamera()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func prepareCamera() {
+        createCaptureSession()
+        configureCaptureDevices()
+        configureDeviceInputs()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func createCaptureSession() {
+        self.captureSession = AVCaptureSession()
     }
-    */
 
+    func configureCaptureDevices() {
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera],
+                                                                  mediaType: AVMediaType.video,
+                                                                   position: AVCaptureDevice.Position.unspecified)
+
+        let cameras = discoverySession.devices
+
+        for camera in cameras {
+
+            if camera.position == .front {
+
+                self.frontCamera = camera
+
+            } else if camera.position == .back {
+
+                self.rearCamera = camera
+
+                do {
+
+                    try camera.unlockForConfiguration()
+                    camera.focusMode = .continuousAutoFocus
+                    try camera.lockForConfiguration()
+
+                } catch {
+                    return
+                }
+            }
+        }
+    }
+
+    func configureDeviceInputs() throws {
+
+        guard let captureSession = self.captureSession else {
+            return
+        }
+
+        if let rearCamera = self.rearCamera {
+
+            self.rearCameraInput = try AVCaptureDeviceInput(device: rearCamera)
+
+            if let rearCameraInput = self.rearCameraInput {
+
+                if captureSession.canAddInput(rearCameraInput) {
+                    captureSession.addInput(rearCameraInput)
+
+                    self.currentCameraPosition = .rear
+                }
+            }
+
+        } else if let frontCamera = self.frontCamera {
+
+            self.frontCameraInput = try AVCaptureDeviceInput(device: frontCamera)
+
+            if let frontCameraInput = self.frontCameraInput {
+
+                if captureSession.canAddInput(frontCameraInput) {
+                    captureSession.addInput(frontCameraInput)
+                }
+
+                self.currentCameraPosition = .front
+            }
+
+        }
+    }
 }
+
+extension CameraViewController: Identifiable {}
