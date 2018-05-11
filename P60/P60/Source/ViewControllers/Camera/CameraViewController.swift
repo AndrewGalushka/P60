891cbec5ee1ce6bacba2bd56628344227a476e45
AndrewGalushka <andrewgalushka@yahoo.com>
2018-05-11 11:00:14 +0300

    rf

--- P60/P60/Source/ViewControllers/Camera/CameraViewController.swift
+++ P60/P60/Source/ViewControllers/Camera/CameraViewController.swift
@@ -16,6 +16,9 @@ enum CameraPosition {
 
 class CameraViewController: BaseViewController {
     
+    
+    @IBOutlet weak var previewLayerContainerView: UIView!
+    
     var captureSession: AVCaptureSession?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
@@ -25,7 +28,11 @@ class CameraViewController: BaseViewController {
 
     var rearCameraInput: AVCaptureDeviceInput?
     var frontCameraInput: AVCaptureDeviceInput?
+    
+    var photoOutput: AVCapturePhotoOutput?
 
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
     init() {
         super.init(nibName: CameraViewController.identifier, bundle: nil)
     }
@@ -36,84 +43,166 @@ class CameraViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.previewLayerContainerView.layer.borderWidth = 3
+        self.previewLayerContainerView.layer.borderColor = UIColor.randomColor.cgColor
+        
         prepareCamera()
     }
-
-    func prepareCamera() {
-        createCaptureSession()
-        configureCaptureDevices()
-        configureDeviceInputs()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        displayPreview()
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        self.previewLayer?.frame = self.previewLayerContainerView.bounds
+    }
+    
+    func displayPreview() {
+        
+        guard
+            let captureSession = self.captureSession,
+            captureSession.isRunning
+        else {
+            return
+        }
+        
+        
+        if previewLayer != nil {
+            previewLayer?.removeFromSuperlayer()
+        }
+        
+        self.previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        guard let previewLayer = self.previewLayer else { return }
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer.connection?.videoOrientation = .portrait
+        
+        self.previewLayerContainerView.layer.insertSublayer(previewLayer, at: 0)
+    }
+    
+    @IBAction func backButtonTouchUpInsideActionHandler(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
 
+extension CameraViewController {
+    
+}
+
+extension CameraViewController {
+    
+    func prepareCamera() {
+        
+        DispatchQueue(label: "CameraViewController.prepareCamera").async {
+            do {
+                
+                self.createCaptureSession()
+                self.configureCaptureDevices()
+                try self.configureDeviceInputs()
+                try self.configurePhotoOutput()
+                
+            } catch {
+                print("error")
+            }
+        }
+    }
+    
     func createCaptureSession() {
         self.captureSession = AVCaptureSession()
     }
-
+    
     func configureCaptureDevices() {
         let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera],
-                                                                  mediaType: AVMediaType.video,
-                                                                   position: AVCaptureDevice.Position.unspecified)
-
+                                                                mediaType: AVMediaType.video,
+                                                                position: AVCaptureDevice.Position.unspecified)
+        
         let cameras = discoverySession.devices
-
+        
         for camera in cameras {
-
+            
             if camera.position == .front {
-
+                
                 self.frontCamera = camera
-
+                
             } else if camera.position == .back {
-
+                
                 self.rearCamera = camera
-
+                
                 do {
-
-                    try camera.unlockForConfiguration()
-                    camera.focusMode = .continuousAutoFocus
-                    try camera.lockForConfiguration()
-
+                    
+//                    try camera.unlockForConfiguration()
+//                    camera.focusMode = .continuousAutoFocus
+//                    try camera.lockForConfiguration()
+                    
                 } catch {
                     return
                 }
             }
         }
     }
-
+    
     func configureDeviceInputs() throws {
-
+        
         guard let captureSession = self.captureSession else {
             return
         }
-
+        
         if let rearCamera = self.rearCamera {
-
+            
             self.rearCameraInput = try AVCaptureDeviceInput(device: rearCamera)
-
+            
             if let rearCameraInput = self.rearCameraInput {
-
+                
                 if captureSession.canAddInput(rearCameraInput) {
                     captureSession.addInput(rearCameraInput)
-
+                    
                     self.currentCameraPosition = .rear
                 }
             }
-
+            
         } else if let frontCamera = self.frontCamera {
-
+            
             self.frontCameraInput = try AVCaptureDeviceInput(device: frontCamera)
-
+            
             if let frontCameraInput = self.frontCameraInput {
-
+                
                 if captureSession.canAddInput(frontCameraInput) {
                     captureSession.addInput(frontCameraInput)
                 }
-
+                
                 self.currentCameraPosition = .front
             }
-
+            
         }
     }
+    
+    func configurePhotoOutput() throws {
+        
+        guard let captureSession = self.captureSession else {
+            return
+        }
+        
+        self.photoOutput = AVCapturePhotoOutput()
+        let settings = AVCapturePhotoSettings()
+        settings.livePhotoVideoCodecType = .jpeg
+        
+        photoOutput?.capturePhoto(with: settings, delegate: self)
+       
+        if let photoOutput = self.photoOutput,
+            captureSession.canAddOutput(photoOutput) {
+            captureSession.addOutput(photoOutput)
+            
+            captureSession.startRunning()
+        }
+    }
+}
+
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    
 }
 
 extension CameraViewController: Identifiable {}
